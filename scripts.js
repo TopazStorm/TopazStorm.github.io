1e6191bb840a04af6f6fd181f42bfd1dab61bc50
TopazStorm <142182719+TopazStorm@users.noreply.github.com>
2023-08-20 04:28:44 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-// Add JavaScript functionality if needed
+// Add JavaScript functionality
+const gameLinks = document.querySelectorAll('.game-list a');
+const websiteTitle = document.getElementById('website-title');
+
+gameLinks.forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        const gameName = link.textContent;
+        websiteTitle.textContent = gameName + " Guides";
+        // You can also update the main content based on the clicked game here
+    });
+});
+
